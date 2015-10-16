9a35fcd10781d1a6ed6ecc1d75404cb49a62c859
Admin <chihong9966@gmail.com>
2015-10-16 10:39:13 +0800

    initial commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxApk = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.textBoxCommand = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonInstall = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(371, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBoxApk
+            // 
+            this.textBoxApk.Location = new System.Drawing.Point(26, 12);
+            this.textBoxApk.Name = "textBoxApk";
+            this.textBoxApk.Size = new System.Drawing.Size(410, 21);
+            this.textBoxApk.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(442, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(51, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "open";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.BackColor = System.Drawing.SystemColors.Info;
+            this.textBoxOutput.Location = new System.Drawing.Point(26, 175);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ReadOnly = true;
+            this.textBoxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxOutput.Size = new System.Drawing.Size(467, 232);
+            this.textBoxOutput.TabIndex = 4;
+            // 
+            // textBoxCommand
+            // 
+            this.textBoxCommand.Location = new System.Drawing.Point(26, 122);
+            this.textBoxCommand.Name = "textBoxCommand";
+            this.textBoxCommand.Size = new System.Drawing.Size(325, 21);
+            this.textBoxCommand.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Command";
+            // 
+            // buttonInstall
+            // 
+            this.buttonInstall.Location = new System.Drawing.Point(371, 54);
+            this.buttonInstall.Name = "buttonInstall";
+            this.buttonInstall.Size = new System.Drawing.Size(122, 39);
+            this.buttonInstall.TabIndex = 7;
+            this.buttonInstall.Text = "Install";
+            this.buttonInstall.UseVisualStyleBackColor = true;
+            this.buttonInstall.Click += new System.EventHandler(this.buttonInstall_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(371, 413);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 39);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 460);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.buttonInstall);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxCommand);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBoxApk);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBoxApk;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.TextBox textBoxCommand;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonInstall;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Windows.Forms;
+using RegawMOD.Android;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        AndroidController android;
+        Device device;
+        string serial;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            android = AndroidController.Instance;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //label1.Text = "clicked";
+          
+
+            //Always call UpdateDeviceList() before using AndroidController on devices to get the most updated list
+            android.UpdateDeviceList();
+
+            if (android.HasConnectedDevices)
+            {
+                serial = android.ConnectedDevices[0];
+                device = android.GetConnectedDevice(serial);
+                //this.textBox2.Text = device.SerialNumber;
+                //AdbCommand adbCmd = Adb.FormAdbShellCommand(this.device, false, "dumpsys", "battery");
+                
+                AdbCommand adbCmd = Adb.FormAdbCommand(textBoxCommand.Text);
+                string result = Adb.ExecuteAdbCommand(adbCmd);
+                textBoxOutput.AppendText(result+"\r\n");
+            }
+            else
+            {
+                this.textBoxOutput.Text = "Error - No Devices Connected";
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Apk Files (.apk)|*.apk|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+
+            openFileDialog1.Multiselect = false;
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // Assign the cursor in the Stream to the Form's Cursor property.
+                textBoxApk.Text = openFileDialog1.FileName;
+            }
+        }
+
+
+
+        private void buttonInstall_Click(object sender, EventArgs e)
+        {
+            serial = android.ConnectedDevices[0];
+            device = android.GetConnectedDevice(serial);
+            string apkFileName = textBoxApk.Text;
+            AdbCommand adbCmd = Adb.FormAdbCommand("install " + apkFileName);
+            string result = Adb.ExecuteAdbCommand(adbCmd);
+            textBoxOutput.AppendText(result + "\r\n");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBoxOutput.Clear();
+        }
+    }
+}
